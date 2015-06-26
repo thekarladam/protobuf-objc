37fff194456f62030f2968ed83ae766e8b908567
Karl Adam <karladam@mac.com>
2015-06-26 16:06:54 -0700

    Static Library -> Dynamic Framework w/ Module

--- src/runtime/Classes/ProtocolBuffers.h
+++ src/runtime/Classes/ProtocolBuffers.h
@@ -16,6 +16,17 @@
 // limitations under the License.
 
 #import <Foundation/Foundation.h>
+
+//! Project version number for ProtocolBuffers.
+FOUNDATION_EXPORT double ProtocolBuffersVersionNumber;
+
+//! Project version string for ProtocolBuffers.
+FOUNDATION_EXPORT const unsigned char ProtocolBuffersVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <ProtocolBuffers/PublicHeader.h>
+
+
+
 #import "Bootstrap.h"
 #import "AbstractMessage.h"
 #import "AbstractMessageBuilder.h"
